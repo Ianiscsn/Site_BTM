ccc114e4de5deee8a38fee324fa8bd2b8c6397d0
Ianis Cousin <ianis.cousin1@gmail.com>
2025-03-31 14:34:52 -0300

    v2

--- ecran.js
+++ ecran.js
@@ -1,6 +1,6 @@
 function checkScreenSize() {
     if (window.innerWidth < 768) {
-        document.querySelectorAll(".contact1, .contact2, .contact3").forEach(el => el.remove());
+        document.querySelectorAll(".contact1, .contact2, .contact3, .texte_trans, .texte_dem, .texte_mess").forEach(el => el.remove());
     }
 }
 
